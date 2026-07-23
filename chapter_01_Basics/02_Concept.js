c5490dd2bfe6eca247f46ed52792ad00f3168140
ndtest-playwright <ndtest-playwright@example.com>
2026-07-23 18:11:13 +0530

    change to 10

--- chapter_01_Basics/02_Concept.js
+++ chapter_01_Basics/02_Concept.js
@@ -1,11 +1,11 @@
 let x = 10;
 console.log(x);
 
-for (let a = 0 a < 100000 a++) {
+for (let a = 0; a < 10; a++) {
 console.log(a);
 print();
 }
 I
 function print() {
-console.log("Hello");
+console.log("Hello Nilesh");
 }
