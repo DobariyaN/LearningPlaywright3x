77e3a55a85bb3197c3695b047607504b7f39be5c
ndtest-playwright <ndtest-playwright@example.com>
2026-07-23 16:45:03 +0530

    Statment and Loop

---
+++ chapter_01_Basics/03_greets_name.js
@@ -0,0 +1,5 @@
+// Prompt the user to enter their name
+const name = prompt("NILESH:");
+
+// Display a greeting
+console.log(`Hello, ${name}!`);
---
+++ chapter_01_Basics/04_Table.js
@@ -0,0 +1,8 @@
+// Input
+const num = 3;
+
+// Print multiplication table
+for (let i = 1; i <= 10; i++) 
+{
+    console.log(`${num} x ${i} = ${num * i}`);
+}
